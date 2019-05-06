fb1dde790d615070aba985d4f4e37d663b413d02
dsgvds <felipekdds2011@gmail.com>
2019-05-06 12:14:50 -0300

    atualização

--- app/src/main/java/com/example/aluno/fazer/MainActivity.java
+++ app/src/main/java/com/example/aluno/fazer/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.aluno.fazer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText comida;
+    private Button button;
+    private Button apagar;
+    private TextView lista;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        comida = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.buttonCadastrar);
+        apagar = (Button) findViewById(R.id.buttonApagar);
+        lista = (TextView) findViewById(R.id.lista);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nomeC = comida.getText().toString();
+                String cadastros = lista.getText().toString() +"\n"+nomeC;
+
+
+
+                if (){
+                    Toast.makeText(MainActivity.this, "Cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Erro!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
